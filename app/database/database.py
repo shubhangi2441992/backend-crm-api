11f6f06240560b935821e4f4a3713f1fd307deb0
ShubhangiDeore <deoreshubhangi24@gmail.com>
2026-01-01 17:14:11 +0530

    Upgrade DB from SQLite to PostgreSQL, add .env configuration

--- app/database/database.py
+++ app/database/database.py
@@ -1,11 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base,Session
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL="sqlite:///./crm.db"
+load_dotenv()
 
-engine = create_engine (
-    DATABASE_URL, connect_args={"check_same_thread":False}
-)
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# DATABASE_URL="sqlite:///./crm.db"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine (DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
